f4f59729a1c8a6ccf803b7385a37b722d8415b49
MiessanEmmanuel <emmanueljeanmiessan@gamil.com>
2023-03-28 19:24:31 -0700

    refont accueil

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -4,16 +4,41 @@
 <main>
     <section class="home_presentation">
         <div class="container">
-            <div class="presentation_msg ">
-                <h1> <span>JAE</span> 
-POUR LE REPOSITIONNEMENT DE L'AFRIQUE</h1>
-                <p>Lorem ipsum dolor sit amet Lorem, ipsum dolor ipsum dolor.<p>
-                <button class="btn btn-primary"> Rejoindre </button>
+            <div class="row">
+                <p class="filigrane filigrane4">JAE</span> </p>
+                <p class="filigrane filigrane1">JAE</span> </p>
+                <p class="filigrane filigrane2">JAE</span> </p>
+                <p class="filigrane filigrane3">JAE</span> </p>
+                <div class="presentation_msg col-12 col-md order-md-1 order-2">
+                    <h1 class="presentation_title ">  <span class="titre">JA<span class="presentation_E">E</span> </h1>
+                    <h2 class="presentation_subtitle fs-3">Pour le repositionnement de l'Afrique</h2>
+                    <p class="presentation_description">Lorem ipsum dolor sit amet Lorem, ipsum dolor ipsum dolor Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur.<p>
+                    <a  class="btn button btn_presentation"> S'inscrire &Aacute; Nos Programmes</a>
+                </div>
+                <div class="presentatin_img  col-12 col-md-7 order-md-2 order-1">
+                    <img src="{{asset('img/membership-circles.png')}}" alt="image de la communauté">
+                </div>
             </div>
         </div>
-     
-     
     </section>
+    <section class="objectif">
+        <div class="container">
+            <div class="">
+                <h2>Objectifs de la JAE</h2>
+            </div>
+            <div class="row">
+                <div class="col-7">
+                    <h3> dolor sapiente</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe non quisquam, aliquid, et quis debitis, voluptates ducimus quidem labore repellendus officiis eveniet nihil veniam? Voluptatem tenetur reiciendis maiores sed rem.</p>
+                </div>
+                <div class="col">
+                    <img src="{{asset('')}}" alt="">
+                </div>
+            </div>
+        </div>
+        
+    </section>
+   
 </main>
    
 @endsection('content')
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
         <!-- link css(public) -->
         <link rel="stylesheet" href="{{asset('css/style.css')}}">
         <!-- link bootstrap -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
@@ -39,7 +39,7 @@
 <script>
   
 </script> 
-       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>
         
