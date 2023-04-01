8649c37f50bfa0791e0e2ea969e5d2be6bfa9728
MiessanEmmanuel <emmanueljeanmiessan@gamil.com>
2023-04-01 07:41:29 -0700

    footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
         <link rel="stylesheet" href="{{asset('css/style.css')}}">
         <!-- link bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
@@ -24,9 +26,11 @@
    
 
 
-@include('partials.header') 
+@include('partials.header')
+
 @yield('content')
 
+@include('partials.footer') 
 
 
 <script src="{{asset('js/app.js')}}"></script>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,58 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+
+<div class="footer-top">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-lg-3 col-md-6 footer-contact footer-links">
+      <a href="/" class="logo me-auto"><img src="{{asset('img/logo_header.png')}}" alt="oups"  style="width: 100px;"></a>
+        <p>
+        <br>
+          <strong>Téléphone:</strong><a  target="_blank" rel="noopener noreferrer">+1 234567865<br>
+          <strong>Email:</strong><a href="mailto:info@jae.com"  target="_blank" rel="noopener noreferrer" > info@jae.com</a> <br>
+        </p>
+      </div>
+      
+      <div class="col-lg-3 col-md-6 footer-links">
+        <h4>Liens utiles</h4>
+        <ul>
+          <li><i class="bi bi-chevron-right"></i><a href="{{route('apropos')}}" class="">A Propos</a></li>
+          <li><i class="bi bi-chevron-right"></i><a href="{{route('programme')}}">Nos Programmes</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="{{route('blog')}}">Blog</a></li>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 footer-links">
+        <h4> Nos Programmes</h4>
+        <ul>
+          <li><i class="bi bi-chevron-right"></i> <a href="/ecommerce">Programme 1</a></li>
+          <li><i class="bi bi-chevron-right"></i><a href="/consulting">Programme 2</a></li>
+          <li><i class="bi bi-chevron-right"></i><a href="/socialwork">Programme 3</a></li>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 footer-links">
+        <h4>Nos réseaux sociaux</h4>
+        <p>Rejoignez-nous sur nos différents réseaux sociaux</p>
+        <div class="social-links mt-3">
+          <a href="https://twitter.com/lenoiresthomme" class="twitter" target="_blank" rel="noopener noreferrer"><i class="bi bi-twitter"></i></a>
+          <a href="https://www.facebook.com/" class="facebook" target="_blank" rel="noopener noreferrer"><i class="bi bi-facebook"></i></a>
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" class="instagram"><i class="bi bi-instagram"></i></a>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<div class="container footer-bottom clearfix">
+  <div class="copyright">
+    &copy; Copyright <strong><span>JAE</span></strong>. All Rights Reserved
+  </div>
+  <div class="credits">
+    Designed by <a href="/">JAE</a>
+  </div>
+</div>
+
+</footer><!-- End Footer -->
